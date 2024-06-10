da3f23e6659cba366221a85fe9b420b837432975
Alefer Reinert <aleferreinert@gmail.com>
2024-06-10 14:35:24 -0300

    Config cors

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,9 +2,17 @@ import { BookRoutes } from '../../src/infra/routes/books.routes'
 
 const serverless = require('serverless-http')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
 require('dotenv').config()
+
+// Origens permitidas
+app.use(
+	cors({
+		origin: 'https://findbook-ar.netlify.app'
+	})
+)
 
 // Conectar ao MongoDB
 mongoose.connect(process.env.DATABASE_URL, {
