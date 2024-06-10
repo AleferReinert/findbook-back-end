4d8367d0ac8c7b2c90d41f28f405e8e4c33623bd
Alefer Reinert <aleferreinert@gmail.com>
2024-06-10 14:04:28 -0300

    Update server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,17 +1,42 @@
-import { BookRoutes } from '../../src/infra/routes/books.routes'
+// import { BookRoutes } from '../../src/infra/routes/books.routes'
 
-const express = require('express')
+// const express = require('express')
+// const serverless = require('serverless-http')
+// const cors = require('cors')
+
+// const app = express()
+
+// app.use(cors())
+// app.use(express.json())
+
+// const router = express.Router()
+// BookRoutes(router)
+
+// app.use('/.netlify/functions/server', router)
+
+// module.exports.handler = serverless(app)
+
 const serverless = require('serverless-http')
-const cors = require('cors')
-
+const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-app.use(cors())
+// Conectar ao MongoDB
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+
+// Middleware
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-const router = express.Router()
-BookRoutes(router)
+// Definir rotas
+app.get('/', (req, res) => {
+	res.send('Hello from Express on Netlify Functions!')
+})
 
-app.use('/.netlify/functions/server', router)
+// Outras rotas aqui
 
 module.exports.handler = serverless(app)
